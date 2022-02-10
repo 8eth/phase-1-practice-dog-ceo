4775cf3de3f947c7c5802f76050d0f87274ccbdc
Betelihem <Betelihem>
2022-02-09 23:07:10 -0800

    commit tree 481e693bcb2d826e2d72a4dcd94b38b833e4dbfb

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+// DOM selector && created elements
+const list = document.querySelector('#dog-image-container')
+const breedList = document.querySelector('#dog-breeds')
+
+// Register event listeners
+
+// Fetches
+function fetchAllDogs() {
+    return fetch(imgUrl)
+    .then(res => res.json())
+}
+
+function fetchAllBreeds() {
+    return fetch(breedUrl)
+    .then(res2 => res2.json())
+}
+
+// Rendering
+function renderAllDogs(dogsObj) {
+    dogsObj.message.forEach(renderOneDog)
+
+    // console.log(dogsObj)
+}
+
+function renderAllBreads(dogsBreedObj) {
+    Object.keys(dogsBreedObj.message).forEach(renderOneBreed)
+
+}
+
+function renderOneBreed(dogsBreedObj) {
+    // console.log(dogsBreedObj.message)
+    const li = document.createElement('li')
+    
+    li.innerText = dogsBreedObj
+    console.log (dogsBreedObj)
+    console.log(li)
+    breedList.appendChild(li) 
+}
+
+function renderOneDog(dogLink) {
+    let img = document.createElement('img')
+    img.src = dogLink
+
+    img.classList.add("img")
+
+    list.appendChild(img)
+}
+
+// Event handler
+
+
+// Initializer(s)
+// fetchAllDogs().then(dogsArr => renderAllDogs(dogsArr))
+fetchAllDogs().then(renderAllDogs)
+fetchAllBreeds().then(renderAllBreads)
